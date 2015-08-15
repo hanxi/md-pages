aa6d1ae696144e748969ed016c9bb96ccce7250e
涵曦 <hanxi.info@gmail.com>
2015-08-15 15:16:41 +0800

    add start script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+netstat -tunlp|grep ":80"|awk '{print $7}'|awk -F/ '{print $1}'|xargs kill -9
+netstat -tunlp|grep ":8000"|awk '{print $7}'|awk -F/ '{print $1}'|xargs kill -9
 cd /md-pages
 python -m SimpleHTTPServer 8000 &
 python /md-pages/http-file-server/file-server.py /md-pages/http-file-server/files 80 &
