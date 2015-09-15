52946cf2e41b865a654d63715c93506ff792b16b
hanxi <hanxi.info@gmail.com>
2015-09-15 13:51:33 +0800

    config for myalauda

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 filepath=$(cd $(dirname $0) && pwd)
 cd $filepath
 python -m SimpleHTTPServer 80 >md-pages.log 2>&1 &
-python ../http-file-server/file-server.py ../http-file-server/files 8001 >file-server.log 2>&1 &
+mkdir -p http-file-server/files/md
+python http-file-server/file-server.py http-file-server/files 8001 >file-server.log 2>&1 &
 
